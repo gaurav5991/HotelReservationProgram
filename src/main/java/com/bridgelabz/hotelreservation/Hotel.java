9e59880ac2a75a1f48732efdcfdfc8fed21536c3
gaurav5991 <gauravenggsharma2017@gmail.com>
2020-10-06 23:35:03 +0530

    [Gaurav]ADDED:Hotel Name and Rate for Regular Customer in Hotel Class

---
+++ src/main/java/com/bridgelabz/hotelreservation/Hotel.java
@@ -0,0 +1,32 @@
+package com.bridgelabz.hotelreservation;
+
+public class Hotel {
+
+    private String hotelName;
+    private int regularRate;
+
+    /*Parameterized Constrictor*/
+
+    public Hotel(String hotelName, int regularRate) {
+        this.hotelName = hotelName;
+        this.regularRate = regularRate;
+    }
+
+    /*Getter And Setter Methods*/
+
+    public String getHotelName() {
+        return hotelName;
+    }
+
+    public void setHotelName(String hotelName) {
+        this.hotelName = hotelName;
+    }
+
+    public int getRegularRate() {
+        return regularRate;
+    }
+
+    public void setRegularRate(int regularRate) {
+        this.regularRate = regularRate;
+    }
+}
